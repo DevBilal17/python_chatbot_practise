20310b31cef332e020b1c35451c6241c1ca34ae6
DevBilal17 <muhammadbilalmb03122@gmail.com>
2026-03-11 01:11:25 +0500

    System, AI, Human Message implemented

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_mistralai import ChatMistralAI
-
+from langchain_core.messages import SystemMessage,AIMessage,HumanMessage
 
 
 model = ChatMistralAI(
@@ -11,6 +11,19 @@ model = ChatMistralAI(
     temperature=0.9
 )
 
-response = model.invoke("Who is Shahid Afridi ?")
+messages = [
+    SystemMessage(content="You are a web developer")
+]
+print("-----------Welcome to the WEB Developer Chat bot---------------")
+print("Type 0 to exit")
+while True:
+    prompt = input("You : ")
+    if(prompt == "0"):
+        break
+    messages.append(HumanMessage(content=prompt))
+    response = model.invoke(messages)
+    print(response.content)
+    messages.append(AIMessage(content=response.content))
 
-print(response.content)
+
+print(messages)
